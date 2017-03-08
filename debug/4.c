61bee9f3b534d2dda9581f77d7c34d244adb739c
Dai Tao <suafeng@gmail.com>
2017-03-08 18:01:21 +0800

    Update 4.c

--- debug/4.c
+++ debug/4.c
@@ -1,6 +1,6 @@
 double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size) {
-    int m, n = 0;
-    int* A, B = NULL;
+     int m, n = 0;
+    int *A, *B = NULL;
     //A <- m, B <- n
     if(nums1Size <= nums2Size){
         m = nums1Size;
@@ -16,7 +16,7 @@ double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Si
     int imax = m;
     int imin = 0;
     int min_right, max_left = 0;
-    
+
     while (imax >= imin){
         int i = (imax + imin) / 2;
         int j = (m + n + 1) / 2 - i;
@@ -27,14 +27,17 @@ double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Si
         }else {
             if(i == m) min_right = B[j];
             else if(j == n) min_right = A[i];
-            else min_right = min(A[i], B[j]);
-            
+            else min_right = (A[i] <= B[j]) ? A[i] : B[j];
+
             if(i == 0) max_left = B[j-1];
             else if(j == 0) max_left = A[i-1];
-            else max_left = max(B[j-1], A[i-1]);
-            
-            if((m + n) % 2 == 1) return double(max_left);
+            else max_left = (B[j-1] <= A[i-1]) ? A[i-1] : B[j-1];
+
+            if((m + n) % 2 == 1) {
+                return (double)max_left;
+            }
             return (max_left + min_right) / 2.0;
         }
     }
+    return (max_left + min_right) / 2.0;
 }
